363e8a46867006028b29eee0564a8aec890ff38d
Danielton Paiva <53627217+danieltonpaiva@users.noreply.github.com>
2024-01-21 18:59:31 -0300

    Update source.py

--- facefusion/uis/components/source.py
+++ facefusion/uis/components/source.py
@@ -35,10 +35,10 @@ def render() -> None:
 		show_label = False
 	)
 	register_ui_component('source_image', SOURCE_IMAGE)
-	arquivos = [f for f in os.listdir('/kaggle/working/facenico3/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico3/exemplos', f))]
+	arquivos = [f for f in os.listdir('/kaggle/working/facenico4/exemplos') if os.path.isfile(os.path.join('/kaggle/working/facenico4/exemplos', f))]
 	files = []
 	for x in arquivos:
-		files.append('/kaggle/working/facenico3/exemplos/' + x)
+		files.append('/kaggle/working/facenico4/exemplos/' + x)
 
 	examples = gradio.Examples(sorted(files), SOURCE_FILE, examples_per_page=20)
 
